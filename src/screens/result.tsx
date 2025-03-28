1c94a8266c67c29865688da52d1974e7017141cf
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-03-28 20:08:22 +0700

    fix: fixing ss name file & using toast istead alert

--- src/screens/result.tsx
+++ src/screens/result.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState, useRef} from 'react';
-import {View, Text, TouchableOpacity, BackHandler, Alert} from 'react-native';
+import {View, Text, TouchableOpacity, BackHandler} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../App'; // Import the ParamList type
 import ConfettiCannon from 'react-native-confetti-cannon'; // Import confetti cannon
 import ViewShot from 'react-native-view-shot'; // Import ViewShot for screenshots
 import RNFS from 'react-native-fs'; // Import RNFS for file handling
+import Toast from 'react-native-toast-message'; // Import Toast for notifications
 import styles from '../styles/result.style';
 
 const ResultScreen: React.FC = () => {
@@ -35,45 +36,57 @@ const ResultScreen: React.FC = () => {
     setTimeout(() => {
       if (viewShotRef.current) {
         (viewShotRef.current as any).capture().then((uri: string) => {
-          const destPath = `${RNFS.DownloadDirectoryPath}/result_screenshot.png`;
+          const timestamp = new Date().getTime(); // Generate a unique timestamp
+          const destPath = `${RNFS.DownloadDirectoryPath}/BisaEnggress_result_${timestamp}.png`; // Add timestamp to file name
           RNFS.moveFile(uri, destPath)
             .then(() => {
-              Alert.alert('Screenshot Tersimpan', `Screenshot disimpan di ${destPath}`);
+              Toast.show({
+                type: 'success',
+                text1: 'Screenshot Tersimpan',
+                text2: `Screenshot disimpan di ${destPath}`,
+              });
             })
             .catch(err => {
-              Alert.alert('Gagal Menyimpan Screenshot', err.message);
+              Toast.show({
+                type: 'error',
+                text1: 'Gagal Menyimpan Screenshot',
+                text2: err.message,
+              });
             })
             .finally(() => setHideButtons(false)); // Show buttons again after saving
         });
       }
-    }, 100); // Delay to ensure buttons are hidden before capturing
+    }, 1); // Delay to ensure buttons are hidden before capturing
   };
 
   return (
-    <ViewShot ref={viewShotRef} options={{format: 'png', quality: 0.9}} style={{flex: 1}}>
-      <View style={styles.container}>
-        {showConfetti && <ConfettiCannon count={100} origin={{x: 0, y: 0}} />}
-        <Text style={styles.header}>Bisa Enggress</Text>
-        <View style={styles.content}>
-          <Text style={styles.title}>Hasil</Text>
-          <Text style={styles.contentText}>
-            Selamat! Anda telah menyelesaikan percakapan. Nilai kemampuan bahasa
-            Inggris Anda adalah 85/100.
-            Sepertinya Anda bisa Enggress!
-          </Text>
-          {!hideButtons && ( // Conditionally render buttons
-            <View style={styles.actionsButtons}>
-              <TouchableOpacity style={styles.button} activeOpacity={0.6} onPress={takeScreenshot}>
-                <Text style={styles.buttonText}>Screenshot</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button} activeOpacity={0.6} onPress={() => navigation.navigate('Home')}>
-                <Text style={styles.buttonText}>Home</Text>
-              </TouchableOpacity>
-            </View>
-          )}
+    <>
+      <ViewShot ref={viewShotRef} options={{format: 'png', quality: 0.9}} style={{flex: 1}}>
+        <View style={styles.container}>
+          {showConfetti && <ConfettiCannon count={100} origin={{x: 0, y: 0}} />}
+          <Text style={styles.header}>Bisa Enggress</Text>
+          <View style={styles.content}>
+            <Text style={styles.title}>Hasil</Text>
+            <Text style={styles.contentText}>
+              Selamat! Anda telah menyelesaikan percakapan. Nilai kemampuan bahasa
+              Inggris Anda adalah 85/100.
+              Sepertinya Anda Bisa Enggress!
+            </Text>
+            {!hideButtons && (
+              <View style={styles.actionsButtons}>
+                <TouchableOpacity style={styles.button} activeOpacity={0.6} onPress={takeScreenshot}>
+                  <Text style={styles.buttonText}>Screenshot</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} activeOpacity={0.6} onPress={() => navigation.navigate('Home')}>
+                  <Text style={styles.buttonText}>Home</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
         </View>
-      </View>
-    </ViewShot>
+      </ViewShot>
+      <Toast />
+    </>
   );
 };
 
